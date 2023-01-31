00164ea074e09f6d228ebbdf041e89dd7868539e
Dave Voutila <dave.voutila@neotechnology.com>
2023-01-31 14:30:58 -0500

    stub out metric descriptor creation

--- gcp.py
+++ gcp.py
@@ -1,10 +1,13 @@
+from enum import Enum
 import logging
 
 import requests
 
-from google.api import label_pb2 as ga_label
-from google.api import metric_pb2 as ga_metric
-from google.cloud import monitoring_v3 as ga_monitoring
+from google.api.label_pb2 import LabelDescriptor
+from google.api.metric_pb2 import Metric, MetricDescriptor
+from google.cloud import monitoring_v3
+
+from typing import cast, Any, Awaitable, List, NamedTuple
 
 
 _METAROOT = "http://metadata.google.internal/computeMetadata/v1"
@@ -12,6 +15,34 @@ _PROJECT_ID = None
 _INSTANCE_ID = None
 _ZONE_ID = None
 _CLIENT = None
+
+_METRIC_TYPE_ROOT = "custom.googleapis.com/neo4j"
+
+
+## TODO: pull these out as a global abstraction
+class MetricKind(Enum):
+    COUNTER = "COUNTER"
+    DELTA = "DELTA"
+    GAUGE = "GAUGE"
+    UNDEFINED = "UNDEFINED"
+
+class MetricType(Enum):
+    BOOL = "BOOL"
+    DISTRIBUTION = "DISTRIBUTION"
+    INT = "INT"
+    FLOAT = "FLOAT"
+    STRING = "STRING"
+    UNDEFINED = "UNDEFINED"
+
+class LabelValueType(Enum):
+    BOOL = "BOOL"
+    INT = "INT"
+    STRING = "STRING"
+
+class MetricLabel(NamedTuple):
+    key: str
+    value_type: LabelValueType = LabelValueType.STRING
+    description: str = ""
 
 
 def getProjectId() -> str:
@@ -23,8 +54,12 @@ def getProjectId() -> str:
                 headers={"Metadata-Flavor": "Google"}
             ).text
         except Exception as e:
-            logger.warning("failed to fetch project id: {e}")
-    return _PROJECT_ID
+            logging.warning(f"failed to fetch project id: {e}")
+    return cast(str, _PROJECT_ID)
+
+
+def getProjectName() -> str:
+    return f"projects/${getProjectId()}"
 
 
 def getInstanceId() -> str:
@@ -36,8 +71,8 @@ def getInstanceId() -> str:
                 headers={"Metadata-Flavor": "Google"}
             ).text
         except Exception as e:
-            logger.warning("failed to fetch instance id: {e}")
-    return _INSTANCE_ID
+            logging.warning(f"failed to fetch instance id: {e}")
+    return cast(str, _INSTANCE_ID)
 
 
 def getZoneId() -> str:
@@ -49,5 +84,59 @@ def getZoneId() -> str:
                 headers={"Metadata-Flavor": "Google"}
             ).text
         except Exception as e:
-            logger.warning("failed to fetch zone id: {e}")
-    return _ZONE_ID
+            logging.warning(f"failed to fetch zone id: {e}")
+    return cast(str, _ZONE_ID)
+
+
+def getClient() -> monitoring_v3.MetricServiceAsyncClient:
+    global _CLIENT
+    if _CLIENT is None:
+        try:
+            _CLIENT = monitoring_v3.MetricServiceAsyncClient()
+        except Exception as e:
+            logging.warning(f"failed to create MetricServiceAsyncClient: {e}")
+    return cast(monitoring_v3.MetricServiceAsyncClient, _CLIENT)
+
+
+async def create_metric_descriptor(name: str, metric_kind: MetricKind, value_type: MetricType,
+                                   labels: List[MetricLabel] = []) -> Awaitable[Any]:
+    client = getClient()
+    desc = MetricDescriptor()
+    desc.type = f"{_METRIC_TYPE_ROOT}/{name}"
+
+    if metric_kind == MetricKind.COUNTER:
+        desc.metric_kind = MetricDescriptor.MetricKind.CUMULATIVE
+    elif metric_kind == MetricKind.DELTA:
+        desc.metric_kind = MetricDescriptor.MetricKind.DELTA
+    elif metric_kind == MetricKind.GAUGE:
+        desc.metric_kind = MetricDescriptor.MetricKind.GAUGE
+    else:
+        desc.metric_kind = MetricDescriptor.MetricKind.METRIC_KIND_UNSPECIFIED
+
+    if value_type == MetricType.BOOL:
+        desc.value_type = MetricDescriptor.ValueType.BOOL
+    elif value_type == MetricType.INT:
+        desc.value_type = MetricDescriptor.ValueType.INT
+    elif value_type == MetricType.FLOAT:
+        desc.value_type = MetricDescriptor.ValueType.DOUBLE
+    elif value_type == MetricType.DISTRIBUTION:
+        desc.value_type = MetricDescriptor.ValueType.DISTRIBUTION
+    elif value_type == MetricType.STRING:
+        desc.value_type = MetricDescriptor.ValueType.STRING
+    else:
+        desc.value_type = MetricDescriptor.ValueType.VALUE_TYPE_UNSPECIFIED
+
+    for label in labels:
+        l = LabelDescriptor()
+        l.key = label.key
+        if label.value_type == LabelValueType.BOOL:
+            l.value_type = LabelDescriptor.ValueType.BOOL
+        elif label.value_type == LabelValueType.INT:
+            l.value_type = LabelDescriptor.ValueType.INT
+        else:
+            l.value_type = LabelDescriptor.ValueType.STRING
+        desc.labels.append(l)
+
+    return client.create_metric_descriptor(
+        name=getProjectName(), metric_descriptor=desc
+    )
