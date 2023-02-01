5ed8db2fd57a29b2b5dc2aea3d1f8da68a24339f
Dave Voutila <dave.voutila@neotechnology.com>
2023-02-01 17:02:48 -0500

    add in argparse

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import argparse
+import asyncio
 import logging
-from time import gmtime
+import logging.config
 
+from time import gmtime
+from typing import Any, Dict, Union
 
 class UTCFormatter(logging.Formatter):
     """
@@ -11,7 +15,7 @@ class UTCFormatter(logging.Formatter):
 
 
 # Mimic Neo4j logging style as best as possible
-LOGGING_CONFIG = {
+LOGGING_CONFIG: Dict[str, Any] = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
@@ -27,17 +31,33 @@ LOGGING_CONFIG = {
             "formatter": "utc",
         },
     },
-    "root": { "handlers": [ "console" ], "level": "DEBUG" },
+    "root": { "handlers": [ "console" ], "level": "INFO" },
 }
 
 
 if __name__ == "__main__":
-    import asyncio
     import relay
-    import logging.config
+
+    parser = argparse.ArgumentParser(
+        prog="metrics-relay",
+        description="Sends Neo4j metrics to the Cloud",
+        epilog="be kind, rewind")
+    parser.add_argument("-d", "--debug", dest="debug", action="store_true",
+                        help="turn on debug logging")
+    parser.add_argument("--host", dest="host",
+                        default="localhost", help="hostname or ip to listen on")
+    parser.add_argument("--port", dest="port", type=int,
+                        default=2003, help="tcp port to listen on")
     try:
+        args = parser.parse_args()
+
+        if args.debug:
+            root = LOGGING_CONFIG["root"]
+            root.update({"level", "DEBUG"})
+            LOGGING_CONFIG.update(root)
         logging.config.dictConfig(LOGGING_CONFIG)
-        asyncio.run(relay.main())
+
+        asyncio.run(relay.main(args.host, args.port))
     except KeyboardInterrupt:
         # XXX ctrl-c caught...we should flush
         pass
